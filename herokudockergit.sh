67216a604cfb1648d93ab99a8bee3e8eb56e02da
l0d0v1c <pub@avaricum.net>
2021-01-01 14:11:15 +0100

    Create herokudockergit.sh

---
+++ herokudockergit.sh
@@ -0,0 +1,11 @@
+
+git config --global credential.helper 'cache --timeout=3600'
+sudo apt update
+sudo apt install joe
+sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository  "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo snap install --classic heroku
+
