1c7b3e96050ddaef140400dde0c9649326e70add
l0d0v1c <pub@avaricum.net>
2021-01-19 11:00:48 +0100

    Create ovhcuda.sh

---
+++ ovhcuda.sh
@@ -0,0 +1,17 @@
+export LC_ALL=C
+sudo add-apt-repository ppa:graphics-drivers
+sudo apt-get update
+sudo add-apt-repository ppa:graphics-drivers/ppa
+sudo apt-get update
+sudo apt install ubuntu-drivers-common
+sudo ubuntu-drivers autoinstall
+lsmod | grep nvidia
+sudo modprobe nvidia
+lsmod | grep nvidia
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh
+chmod +x miniconda.sh
+./miniconda.sh
+source .bashrc
+conda create -n tgpu tensorflow-gpu
+conda activate tgpu
+conda install pandas pillow imageio matplotlib requests
