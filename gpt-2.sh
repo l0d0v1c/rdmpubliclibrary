7ee806c5bc06b57bc0e0c24786cbd90392ab498f
l0d0v1c <pub@avaricum.net>
2021-01-19 13:28:13 +0100

    Create gpt-2.sh

---
+++ gpt-2.sh
@@ -0,0 +1,20 @@
+export LC_ALL=C
+sudo add-apt-repository ppa:graphics-drivers
+sudo apt-get update
+sudo add-apt-repository ppa:graphics-drivers/ppa
+sudo apt-get update
+sudo apt install ubuntu-drivers-common
+sudo ubuntu-drivers autoinstall
+lsmod | grep nvidia
+sudo modprobe nvidia
+lsmod | grep nvidia
+echo 'export PATH=/opt/conda/bin:$PATH' > /etc/profile.d/conda.sh && \
+    wget --quiet https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh && \
+    /bin/bash ~/miniconda.sh -b -p /opt/conda && \
+    rm ~/miniconda.sh
+source .bashrc
+conda create -n tgpu python=3.6 tensorflow-gpu==1.15.0
+conda activate tgpu
+conda install -y requests
+sudo apt-get install joe git 
+git config --global credential.helper 'cache --timeout=3600'
