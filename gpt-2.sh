01acf2862f0329ad91655aee6da9aab4c9d47e4a
l0d0v1c <pub@avaricum.net>
2021-01-19 13:46:06 +0100

    Update gpt-2.sh

--- gpt-2.sh
+++ gpt-2.sh
@@ -8,14 +8,13 @@ sudo ubuntu-drivers autoinstall
 lsmod | grep nvidia
 sudo modprobe nvidia
 lsmod | grep nvidia
-echo 'export PATH=/opt/conda/bin:$PATH' > /etc/profile.d/conda.sh && \
-    wget --quiet https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh && \
-    /bin/bash ~/miniconda.sh -b -p /opt/conda && \
-    rm ~/miniconda.sh
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh
+chmod +x miniconda.sh
+bash ./miniconda.sh
 source .bashrc
 conda create -n tgpu python=3.6 tensorflow-gpu==1.15.0
 conda activate tgpu
 conda install -y requests
 pip install gpt-2-simple
-sudo apt-get install joe git 
+sudo apt-get install joe  
 git config --global credential.helper 'cache --timeout=3600'
