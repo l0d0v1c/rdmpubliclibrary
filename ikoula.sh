22d70af05813b5821acf54e482a5cdfb5c97d206
l0d0v1c <pub@avaricum.net>
2021-01-22 14:57:20 +0100

    Update ikoula.sh

--- ikoula.sh
+++ ikoula.sh
@@ -21,3 +21,9 @@ sudo apt-key fingerprint 0EBFCD88
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io
+
+sudo useradd -m dockr
+sudo passwd dockr
+sudo usermod -aG sudo dockr
+
+
