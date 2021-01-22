84d10aa370e872681493bb0bc7a9ca6875e30395
l0d0v1c <pub@avaricum.net>
2021-01-22 18:45:13 +0100

    Create ikoula-step-1.sh

---
+++ ikoula-step-1.sh
@@ -0,0 +1,14 @@
+export LC_ALL=C
+sudo add-apt-repository ppa:graphics-drivers
+sudo apt-get update
+sudo add-apt-repository ppa:graphics-drivers/ppa
+sudo apt-get update
+#sudo apt install ubuntu-drivers-common
+#sudo ubuntu-drivers autoinstall
+#sudo add-apt-repository ppa:graphics-drivers
+#sudo apt-get update
+sudo apt-get install -y nvidia-driver-450
+reboot
+
+
+
