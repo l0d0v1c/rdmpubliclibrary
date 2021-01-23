40ad318acd13dfe4fca91f16cef83c9ebdd2bb03
l0d0v1c <pub@avaricum.net>
2021-01-23 09:19:58 +0100

    Update ikoula-step-2.sh

--- ikoula-step-2.sh
+++ ikoula-step-2.sh
@@ -1,7 +1,7 @@
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh
 chmod +x miniconda.sh
 bash ~/miniconda.sh
-source .bashrc
+source ~/.bashrc
 sudo apt-get install joe git 
 git config --global credential.helper 'cache --timeout=3600'
 
