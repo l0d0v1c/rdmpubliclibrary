0f86a36969cbe1f5e6c08b3b8993de18bef3423c
l0d0v1c <pub@avaricum.net>
2021-03-09 15:25:22 +0100

    Update ikoula-step-1.sh

--- ikoula-step-1.sh
+++ ikoula-step-1.sh
@@ -8,6 +8,9 @@ sudo apt-get update
 #sudo add-apt-repository ppa:graphics-drivers
 #sudo apt-get update
 sudo apt-get install -y nvidia-driver-450
+sudo apt-get install -y fail2ban
+sudo systemctl start fail2ban
+sudo systemctl enable fail2ban
 reboot
 
 
